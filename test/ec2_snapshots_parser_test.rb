9504f82fbd8bc7b5fb548483bc529ecddc178bea
Charles Feduke <charles.feduke@gmail.com>
2014-05-01 10:57:25 -0400

    supports the plain text format sometimes returned by the aws commands

--- test/ec2_snapshots_parser_test.rb
+++ test/ec2_snapshots_parser_test.rb
@@ -2,14 +2,19 @@ require 'test/unit'
 require_relative '../bin/delete-old-ebs-snapshots'
 
 class Ec2SnapshotsParserTest < Test::Unit::TestCase
-  def subject(&block)
-    s = Ec2SnapshotsParser.new(IO.readlines('./test/fixtures/sample-data.txt'))
+  def subject(fixture, &block)
+    s = Ec2SnapshotsParser.new(IO.readlines("./test/fixtures/#{fixture}.txt"))
     block.call(s)
   end
 
-  def test_reads_all_rows
+  def test_reads_all_rows_for_old
     expected = 54
-    subject { |s| assert(s.snapshots.length == expected, "Expected #{expected} snapshots, actually #{s.snapshots.length} snapshots") }
+    subject("sample-data-old") { |s| assert(s.snapshots.length == expected, "Expected #{expected} snapshots, actually #{s.snapshots.length} snapshots") }
+  end
+
+  def test_reads_all_rows_for_new
+    expected = 4205
+    subject("sample-data-new") { |s| assert(s.snapshots.length == expected, "Expected #{expected} snapshots, actually #{s.snapshots.length} snapshots") }
   end
 
 end
